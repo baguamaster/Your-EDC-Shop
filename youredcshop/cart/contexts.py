f3f3a7475e66b6989ec38da834c99fc82f598a61
baguamaster <traderkev54@yahoo.com>
2021-05-17 08:47:16 +0000

    troubleshooting cart2

---
+++ youredcshop/cart/contexts.py
@@ -0,0 +1,8 @@
+  
+def cart_contents(request):
+    # retrieve shopping cart from session
+    cart = request.session.get('shopping_cart', {})
+    return {
+        'shopping_cart': cart,
+        'items_in_cart': len(cart)
+    }
