caf69b19fb8d148c8c5ccf6250981aef4da9a397
baguamaster <traderkev54@yahoo.com>
2021-06-30 07:02:11 +0000

    js for toastr optionalities2

---
+++ youredcshop/static/script.js
@@ -0,0 +1,17 @@
+// Toaster options
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-full-width",
+    "preventDuplicates": true,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
