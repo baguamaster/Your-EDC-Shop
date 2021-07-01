b58a0acdf24154bcf9da43dd4cd99311ef28a519
baguamaster <traderkev54@yahoo.com>
2021-07-01 10:20:38 +0800

    structure changes

---
+++ products/forms.py
@@ -0,0 +1,26 @@
+# ProductForm
+from django import forms
+from .models import Product, ProductType
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'name',
+            'product_type',
+            # 'image',
+            'price',
+            'colour',
+            'tag',
+            'length',
+            'desc',
+            'quantity'
+        )
+
+
+# normal form
+class SearchForm(forms.Form):
+    name = forms.CharField(max_length=100, required=False)
+    product_type = forms.ModelChoiceField(
+        queryset=ProductType.objects.all(), required=False)
